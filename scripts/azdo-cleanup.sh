88993a3915ae500572eb2d3dd1df5b7d90ec547e
jldeen <jedeen@outlook.com>
2019-04-16 23:21:38 +0100

    changes

--- scripts/azdo-cleanup.sh
+++ scripts/azdo-cleanup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+# TODO: Fix verification checks
 set -eou pipefail
 source ./scripts/variables.sh
 
 curl -u $(presenter):$(PAT) --request DELETE "https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?forceDelete=true&api-version=5.0" 
 
-if curl -u $(presenter):$(PAT) --request GET "https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?api-version=5.0" ; then echo "$(releaseDef) not deleted. Please check your PAT and try again."
-else echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted."
-fi
+curl -u $(presenter):$(PAT) --request GET "https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?api-version=5.0" && echo "$(releaseDef) not deleted. Please check your PAT and try again." || echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted."
 
 sleep 10
 curl -u $(presenter):$(PAT) --request DELETE "https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0" 
 
-if curl -u $(presenter):$(PAT) --request GET https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0 ; then echo "$(buildDef) not deleted. Please check your PAT and try again."
-else echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted."
-fi
+curl -u $(presenter):$(PAT) --request GET https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0 && echo "$(buildDef) not deleted. Please check your PAT and try again." || echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted."
