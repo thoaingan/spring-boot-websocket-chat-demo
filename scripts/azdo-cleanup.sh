b131386248a022399649b747e2ba253fdd7709c8
jldeen <jedeen@outlook.com>
2019-04-17 13:16:14 +0100

    update verification checks

--- scripts/azdo-cleanup.sh
+++ scripts/azdo-cleanup.sh
@@ -2,8 +2,11 @@
 set -eou pipefail
 source ./scripts/variables.sh
 
-curl -s -u $(presenter):$(PAT) --request DELETE "https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?forceDelete=true&api-version=5.0" | grep 'does not exist' > /dev/null && echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted." || echo "Release definition $(releaseDef) not deleted. Please check your inputs, including your PAT, and try again."
+releaseAPI="https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?forceDelete=true&api-version=5.0"
+buildAPI="https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0"
+
+curl -s -u $(presenter):$(PAT) --request DELETE $releaseAPI > /dev/null && curl -s -u $(presenter):$(PAT) --request GET $releaseAPI | grep 'does not exist' > /dev/null && echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted." || echo "Release definition $(releaseDef) not deleted. Please check your inputs, including your PAT, and try again."
 
 sleep 5
 
-curl -s -u $(presenter):$(PAT) --request DELETE "https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0" | grep 'was not found' > /dev/null && echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted." || echo "Build definition $(buildDef) not deleted. Please check your inputs, including your PAT, and try again."
+curl -s -u $(presenter):$(PAT) --request DELETE $buildAPI > /dev/null && curl -s -u $(presenter):$(PAT) --request GET $buildAPI | grep 'was not found' > /dev/null && echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted." || echo "Build definition $(buildDef) not deleted. Please check your inputs, including your PAT, and try again."
